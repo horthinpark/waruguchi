3e909369da3e43482fe105b36537fd7c90b877a3
Takamasa kawabe <pent23tkms@gmail.com>
2014-11-30 17:02:00 +0900

    気になるボタン追加

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,14 +1,20 @@
 socket = io('http://localhost:9999');
 angular.module('waruguchiApp', [])
   .controller('PostController', ['$scope', function($scope) {
-    $scope.user_name = 'Kawabe';
+    $scope.increment = function(post){
+          post.count += 1;
+            }
 
     $scope.posts = [
-      {targetName: '川辺', content:'がうざい'},
-      {targetName: '川辺', content:'がきもい'}];
+      {targetName: '川辺', content:'がうざい', fav_count: 0},
+      {targetName: '川辺', content:'がきもい', fav_count: 0}];
 
     $scope.addPost = function() {
-      $scope.posts.push({user_name: $scope.userName, content: $scope.postContent});
+      $scope.posts.push({user_name: $scope.userName, content:$scope.postContent, fav_count: 0});
       $scope.postContent = '';
+    };
+    
+    $scope.addFavCount = function(post) {
+      post.fav_count += 1;
     };
 }]);
