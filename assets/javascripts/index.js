6fa2bcf3aec88feb3e6b7a553dfca0ea285dd9ea
Takamasa kawabe <pent23tkms@gmail.com>
2014-11-30 17:53:24 +0900

    favcountが閾値を越したら、名前を表出

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,20 +1,25 @@
 socket = io('http://localhost:9999');
 angular.module('waruguchiApp', [])
   .controller('PostController', ['$scope', function($scope) {
+
+    $scope.THRESHOLD = 5;
+
     $scope.increment = function(post){
           post.count += 1;
-            }
+          if (post.count >= 5) {
+          };
+     };
 
     $scope.posts = [
-      {targetName: '川辺', content:'がうざい', fav_count: 0},
-      {targetName: '川辺', content:'がきもい', fav_count: 0}];
+      {targetName: '川辺', content:'がうざい', favCount: 0},
+      {targetName: '川辺', content:'がきもい', favCount: 0}];
 
     $scope.addPost = function() {
-      $scope.posts.push({user_name: $scope.userName, content:$scope.postContent, fav_count: 0});
+      $scope.posts.push({targetName: $scope.targetName, content:$scope.postContent, favCount: 0});
       $scope.postContent = '';
     };
     
     $scope.addFavCount = function(post) {
-      post.fav_count += 1;
+      post.favCount += 1;
     };
 }]);
