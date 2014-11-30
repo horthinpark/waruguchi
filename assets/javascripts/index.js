bc217a8c42447923daa06de2685f6cb6c8b0ba5b
Takamasa kawabe <pent23tkms@gmail.com>
2014-11-30 19:14:50 +0900

    一度ボタン押されたら、disabledにする

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -4,12 +4,6 @@ angular.module('waruguchiApp', [])
 
     $scope.THRESHOLD = 5;
 
-    $scope.increment = function(post){
-          post.count += 1;
-          if (post.count >= 5) {
-          };
-     };
-
     $scope.posts = [
       {targetName: '川辺', content:'がうざい', favCount: 0},
       {targetName: '川辺', content:'がきもい', favCount: 0}];
@@ -21,5 +15,6 @@ angular.module('waruguchiApp', [])
     
     $scope.addFavCount = function(post) {
       post.favCount += 1;
+      $('.fav-count-button').attr('disabled',true);
     };
 }]);
