fabbaa786f90460b72ee45a624414d34e1c62388
Takamasa kawabe <pent23tkms@gmail.com>
2014-11-30 13:55:15 +0900

    post処理完成

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,27 +1,11 @@
 angular.module('waruguchiApp', [])
   .controller('PostController', ['$scope', function($scope) {
-    $scope.todos = [
-      {text:'あいつうざい', done:true},
-      {text:'あいつきもい', done:false}];
+    $scope.posts = [
+      {text:'あいつうざい'},
+      {text:'あいつきもい'}];
 
     $scope.addPost = function() {
-      $scope.todos.push({text:$scope.todoText, done:false});
+      $scope.posts.push({text:$scope.postText});
       $scope.postText = '';
-    };
-
-    $scope.remaining = function() {
-      var count = 0;
-      angular.forEach($scope.todos, function(todo) {
-        count += todo.done ? 0 : 1;
-      });
-      return count;
-    };
-
-    $scope.archive = function() {
-      var oldTodos = $scope.posts;
-      $scope.posts = [];
-      angular.forEach(oldTodos, function(todo) {
-        if (!todo.done) $scope.todos.push(todo);
-      });
     };
 }]);
