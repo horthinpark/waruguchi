7ce50cd55dac2c43cd3e9d50250853da80573a66
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2014-11-30 14:19:05 +0900

    attach user_name

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,6 +1,8 @@
 socket = io('http://localhost:9999');
 angular.module('waruguchiApp', [])
   .controller('PostController', ['$scope', function($scope) {
+    $scope.user_name = 'Kawabe';
+
     $scope.posts = [
       {text:'川辺うざい'},
       {text:'川辺きもい'}];
